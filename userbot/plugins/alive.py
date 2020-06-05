7fe57a1df317640c1723676f1fe8598170395c89
Devil666Satan <62686857+Devil666Satan@users.noreply.github.com>
2020-06-05 09:18:58 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -14,7 +14,7 @@ async def amireallyalive(alive):
     await alive.edit("   **Welcome To DevilBot ** \n\n"
         "**`Hey! I'm alive. All systems online and functioning normally... ψ(｀∇´)ψ`**\n\n"
                       "` 🔸 Telethon version:` **6.9.0**\n` 🔹 Python:` **3.7.3** \n` 🔸 \n"
-                     "` 🔹 Bot created by:` [𝕷𝖎𝖌𝖍𝖙 𝕭𝖗𝖎𝖓𝖌𝖊𝖗 ](tg://user?id=1028626898)\n"
+                     "` 🔹 Bot created by:` [𝕷𝖎𝖌𝖍𝖙 𝕭𝖗𝖎𝖓𝖌𝖊𝖗 ](tg://user?id=417466136)\n"
                      "` 🔸 Database Status:` **All OK 👌!**\n"
                      f"` 🔹 My peru owner`: {DEFAULTUSER}\n\n"
                      "           [✨ GitHub Repository ✨](https://github.com/Devil666Satan/TeleBot)")
